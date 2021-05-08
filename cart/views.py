f7a90178f90ca7c048037e1dd5562b54e1fcd866
Nikita <nikitosan123@gmail.com>
2021-05-08 17:54:37 +0300

    added cart

--- cart/views.py
+++ cart/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Cart
-from django.db.models.signals import pre_save, post_save
-
-def cart_create(user=None):
-    print('New cart created')
-    cart_obj = Cart.objects.create(user=None)
-    return cart_obj 
-
+from .models import Cart, CartManager
 
 def cart_home(request):
-    cart_obj, new_obj = Cart.objects.new_or_get(request, id=cart_obj.id)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
     products = cart_obj.products.all()
     total = 0
     for x in products:
         total += x.price
     print(total)
-
-    return render(request, "cart/cart.html", {'total':total})
+    cart_obj.total = total
+    cart_obj.save()
+    return render(request, "cart/cart.html")
